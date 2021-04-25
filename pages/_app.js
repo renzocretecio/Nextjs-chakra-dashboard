087c343acf0d84220f4dc37b7cc13ac24a3e2e0d
Renzo cretecio <renzocretecio@gmail.com>
2021-04-26 03:44:41 +0800

    sidebar deleted

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../styles/theme'
 import Layout from '../components/Layout'
-import Sidebar from '../components/sidebar'
 
 function MyApp({ Component, pageProps }) {
   return (
